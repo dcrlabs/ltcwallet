c4830714ea037118ef5701caf6f2ef8eb8d48335
yyforyongyu <yong2452@gmail.com>
2025-04-09 20:00:20 +0800

    wallet: use `OutputsToWatch` during startup

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -383,7 +383,7 @@ func (w *Wallet) activeData(dbtx walletdb.ReadWriteTx) ([]ltcutil.Address, []wtx
 		return nil, nil, err
 	}
 
-	unspent, err := w.TxStore.UnspentOutputs(txmgrNs)
+	unspent, err := w.TxStore.OutputsToWatch(txmgrNs)
 	return addrs, unspent, err
 }
 
